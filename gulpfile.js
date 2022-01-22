9995f6d41e7e3f2c98630150362a8bd0f6d2a0e7
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-01-22 15:48:46 +0700

    Updated

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require("gulp");
+const gulpRename = require("gulp-rename");
+const gulpFileInclude = require("gulp-file-include");
+
+gulp.task("build", function () {
+    return gulp.src("./src/base.xml")
+        .pipe(gulpFileInclude({
+            basepath: "@file",
+            indent: true,
+            prefix: "@@"
+        }))
+        .pipe(gulpRename({
+            basename: "webium-theme",
+            extname: ".xml"
+        }))
+        .pipe(gulp.dest("./dist"))
+})
