58c6d9c46767d437fbd3c4172822cee026a6a501
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-01-22 16:26:50 +0700

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,33 @@
 const gulp = require("gulp");
 const gulpRename = require("gulp-rename");
 const gulpFileInclude = require("gulp-file-include");
+const gulpSass = require('gulp-sass')(require('sass'));
+const gulpAutoPrefixer = require('gulp-autoprefixer');
+const gulpCSSMinify = require('gulp-minify-css');
 
-gulp.task("build", function () {
+gulp.task("styles", function () {
+    return gulp.src("./src/assets/styles/*.scss")
+        .pipe(gulpSass())
+        .pipe(gulp.dest("./build/styles"))
+});
+
+gulp.task("styles:autoprefix", function () {
+    return gulp.src("./build/styles/*.css")
+        .pipe(gulpAutoPrefixer({
+            cascade: false
+        }))
+        .pipe(gulp.dest("./build/styles"))
+});
+
+gulp.task("styles:minify", function () {
+    return gulp.src("./build/styles/*.css")
+        .pipe(gulpCSSMinify({
+            keepSpecialComments: 0
+        }))
+        .pipe(gulp.dest("./build/styles"))
+});
+
+gulp.task("start", function () {
     return gulp.src("./src/base.html")
         .pipe(gulpFileInclude({
             basepath: "@file",
@@ -14,4 +39,6 @@ gulp.task("build", function () {
             extname: ".xml"
         }))
         .pipe(gulp.dest("./dist"))
-})
+});
+
+gulp.task("build", gulp.series("styles", "styles:autoprefix", "start"))
