eeb9cb25dba46b957b76cd73cdcbcf59b19ef3b2
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-01-27 22:44:14 +0700

    Updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
+const fs = require("fs");
+const del = require("del");
 const gulp = require("gulp");
 const gulpRename = require("gulp-rename");
 const gulpFileInclude = require("gulp-file-include");
+const gulpTokenReplace = require("gulp-token-replace");
 const gulpSass = require('gulp-sass')(require('sass'));
 const gulpAutoPrefixer = require('gulp-autoprefixer');
 const gulpCSSMinify = require('gulp-minify-css');
@@ -8,7 +11,11 @@ const gulpBabel = require("gulp-babel");
 const gulpBabelMinify = require("gulp-babel-minify");
 
 gulp.task("styles", function () {
-    return gulp.src("./src/assets/styles/*.scss")
+    return gulp.src([
+        "./src/assets/styles/*.scss",
+        "./src/assets/styles/libraries/*.css",
+        "./src/assets/styles/libraries/*.scss"
+    ])
         .pipe(gulpSass())
         .pipe(gulp.dest("./build/styles"))
 });
@@ -48,11 +55,17 @@ gulp.task("scripts:minify", function () {
 });
 
 gulp.task("start", function () {
+    delete require.cache[require.resolve("./config.json")];
+    var config = require("./config.json");
+
     return gulp.src("./src/base.html")
+        .pipe(gulpTokenReplace({
+            global: config
+        }))
         .pipe(gulpFileInclude({
             basepath: "@file",
             indent: true,
-            prefix: "elc:"
+            prefix: "@@"
         }))
         .pipe(gulpRename({
             basename: "webium-theme",
