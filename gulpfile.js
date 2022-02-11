7fa6bf774caa0224847258877521eff48eddbc1e
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-02-11 19:07:41 +0700

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,69 @@
 const del = require("del");
 const gulp = require("gulp");
-const gulpTokenReplace = require("gulp-token-replace");
-const gulpFileInclude = require("gulp-file-include");
+const gulpSass = require('gulp-sass')(require('sass'));
 const gulpRename = require("gulp-rename");
+const gulpMinifyCSS = require("gulp-minify-css");
+const gulpFileInclude = require("gulp-file-include");
+const gulpAutoprefixer = require("gulp-autoprefixer")
+const gulpTokenReplace = require("gulp-token-replace");
 
-// Clean Dist and Build Folder
+const paths = {
+    styles: {
+        src: [
+            "../src/assets/styles/*.css",
+            "./src/assets/styles/*.scss",
+            "./src/assets/styles/libraries/*.css",
+            "./src/assets/styles/libraries*.scss",
+        ],
+        build: [
+            "./build/styles/*.css"
+        ]
+    },
+    scripts: {
+        src: [
+            "./src/assets/scripts/*.js",
+            "./src/assets/scripts/partial/*.js",
+            "./src/assets/scripts/libraries/*.js",
+        ],
+        build: [
+            "./build/scripts/*.js"
+        ]
+    },
+    json: {
+        src: [
+            "./src/assets/schema/*.json"
+        ]
+    }
+};
+
 gulp.task("clean", function () {
     return del(["dist/**", "build/**"], {
         force: true
     });
 });
 
-// Generate XML File
+gulp.task("styles", function () {
+    return gulp.src(paths.styles.src)
+        .pipe(gulpSass())
+        .pipe(gulp.dest("./build/styles"))
+});
+
+gulp.task("styles:autoprefixed", function () {
+    return gulp.src(paths.styles.build)
+        .pipe(gulpAutoprefixer({
+            cascade: false
+        }))
+        .pipe(gulp.dest("./build/styles"))
+});
+
+gulp.task("styles:minify", function () {
+    return gulp.src("./build/styles/*.css")
+        .pipe(gulpMinifyCSS({
+            keepSpecialComments: 0
+        }))
+        .pipe(gulp.dest("./build/styles"))
+});
+
 gulp.task("start", function () {
     delete require.cache[require.resolve("./config.json")];
     var config = require("./config.json");
@@ -34,10 +86,14 @@ gulp.task("start", function () {
 
 gulp.task("build:development", gulp.series(
     "clean",
+    "styles",
     "start"
 ));
 
 gulp.task("build:production", gulp.series(
     "clean",
+    "styles",
+    "styles:autoprefixed",
+    "styles:minify",
     "start"
 ));
