573fe4b1379469933f04f436ca9b330777c47934
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-07-10 11:23:11 +0700

    update 6 files

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
                         }
                     },
                 },
+            },
+            fontFamily: {
+                "webium": 'webium, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";'
             }
         },
     },
