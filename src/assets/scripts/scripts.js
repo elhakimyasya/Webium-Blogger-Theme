8562ddb9eb6ca43e5ab9e945eb5ebe71e805094f
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-07-20 09:26:03 +0700

    update package.json, scripts.js, defaultvariables.xml and Blog1.xml

--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -8,3 +8,39 @@ document.addEventListener("lazybeforeunveil", function (event) {
         event.target.classList.remove("animate-pulse");
     }
 });
+
+// Comment System
+var elcreativeComments = document.querySelector(".elcreative_comment");
+if (elcreativeComments) {
+    var commentSource = elcreativeComments.getAttribute("data-src");
+    var commentIframe = elcreativeComments.querySelector("#comment-editor");
+    var commentAdd = elcreativeComments.querySelector(".add_comment");
+
+    if (commentAdd) {
+        var commentEnable = commentAdd.getAttribute("data-comments");
+        var commentThread = elcreativeComments.querySelector("#threaded_comment_form");
+
+        if (commentIframe.setAttribute("src", commentSource), commentEnable == "true") {
+            var commentReply = elcreativeComments.getElementsByClassName("reply_to");
+            var commentThreadData = commentThread;
+
+            for (let index = 0; index < commentReply.length; index++) {
+                commentReply[index].addEventListener("click", function (event, index) {
+                    var commentReplyTo = this.getAttribute("data-reply-to");
+
+                    elcreativeComments.querySelector(`#c${commentReplyTo}`).appendChild(commentThreadData);
+                    commentIframe.setAttribute("src", `${commentSource}&parentID=${commentReplyTo}`);
+                    commentAdd.classList.add("comment-reply");
+                    commentAdd.classList.remove("hidden");
+                })
+            };
+
+            var commentForm = document.getElementsByClassName("comment_form")[0];
+            commentAdd.addEventListener("click", function () {
+                commentForm.appendChild(commentThreadData);
+                commentAdd.classList.add("hidden");
+                commentIframe.setAttribute("src", commentSource)
+            })
+        }
+    };
+}
