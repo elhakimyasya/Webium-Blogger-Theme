1210b5237655468d546029494463240b9b900394
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-10-10 18:18:43 +0700

    update 5 files

--- src/assets/scripts/components/dropdown.js
+++ src/assets/scripts/components/dropdown.js
@@ -101,23 +101,4 @@ class Dropdown {
 
 window.Dropdown = Dropdown;
 
-function initDropdown() {
-    document.querySelectorAll('[data-dropdown-toggle]').forEach(triggerEl => {
-        const targetEl = document.getElementById(triggerEl.getAttribute('data-dropdown-toggle'))
-        const placement = triggerEl.getAttribute('data-dropdown-placement')
-
-        new Dropdown(targetEl, triggerEl, {
-            placement: placement ? placement : Default.placement
-        })
-    })
-}
-
-if (document.readyState !== 'loading') {
-    // DOMContentLoaded event were already fired. Perform explicit initialization now
-    initDropdown()
-} else {
-    // DOMContentLoaded event not yet fired, attach initialization process to it
-    document.addEventListener('DOMContentLoaded', initDropdown)
-}
-
 export default Dropdown
--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -1,3 +1,21 @@
 import Dropdown from "./components/dropdown";
 
-Dropdown();
+
+function initDropdown() {
+    document.querySelectorAll('[data-dropdown-toggle]').forEach(triggerEl => {
+        const targetEl = document.getElementById(triggerEl.getAttribute('data-dropdown-toggle'))
+        const placement = triggerEl.getAttribute('data-dropdown-placement')
+
+        new Dropdown(targetEl, triggerEl, {
+            placement: placement
+        })
+    })
+}
+
+if (document.readyState !== 'loading') {
+    // DOMContentLoaded event were already fired. Perform explicit initialization now
+    initDropdown()
+} else {
+    // DOMContentLoaded event not yet fired, attach initialization process to it
+    document.addEventListener('DOMContentLoaded', initDropdown)
+}
