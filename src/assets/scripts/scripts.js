3e10b8161b23ca2ace0ee2ad64ce18563f57d089
Yasya El Hakim <yasyaelhakim@gmail.com>
2022-10-30 06:13:36 +0700

    update package.json, scripts.js, defaultvariables.html and Blog1.html

--- src/assets/scripts/scripts.js
+++ src/assets/scripts/scripts.js
@@ -1,3 +1,40 @@
+const initComments = (elcreativeComments) => {
+    const commentContainer = document.querySelector(elcreativeComments);
+    if (commentContainer) {
+        const commentSource = commentContainer.getAttribute('data-src');
+        const commentIframe = commentContainer.querySelector('#comment-editor');
+        const commentAdd = commentContainer.querySelector('.add_comment');
+
+        if (commentAdd) {
+            const commentEnable = commentAdd.getAttribute('data-comments');
+            const commentThread = commentContainer.querySelector('#threaded_comment_form');
+
+            if ((commentIframe.setAttribute('src', commentSource), commentEnable == 'true')) {
+                const commentReply = commentContainer.getElementsByClassName('reply_to');
+                const commentThreadData = commentThread;
+
+                for (let index = 0; index < commentReply.length; index++) {
+                    commentReply[index].addEventListener('click', function () {
+                        const commentReplyTo = this.getAttribute('data-reply-to');
+
+                        commentContainer.querySelector(`#c${commentReplyTo}`).appendChild(commentThreadData);
+                        commentIframe.setAttribute('src', `${commentSource}&parentID=${commentReplyTo}`);
+                        commentAdd.classList.add('comment-reply');
+                        commentAdd.classList.remove('hidden');
+                    });
+                };
+
+                const commentForm = document.getElementsByClassName('comment_form')[0];
+                commentAdd.addEventListener('click', () => {
+                    commentForm.appendChild(commentThreadData);
+                    commentAdd.classList.add('hidden');
+                    commentIframe.setAttribute('src', commentSource);
+                });
+            }
+        }
+    }
+};
+
 // Lazysizes
 document.addEventListener('lazybeforeunveil', (event) => {
     const attrDataImage = event.target.getAttribute('data-image');
@@ -20,6 +57,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 hideTarget && document.querySelector(hideTarget).classList.remove('hidden')
             }
         })
+    });
+
+    document.querySelectorAll('.button_toggle_comment').forEach((elements) => {
+        elements.addEventListener('click', initComments('.elcreative_comment'))
     })
 })
 
